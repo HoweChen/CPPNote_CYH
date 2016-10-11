3a4b45e751f4d533232733dee2d580e845d17ee8
SkyFather <cyh278889789@gmail.com>
2016-10-11 12:12:02 +0800

    const char* const the_string

---
+++ const char* const the_string/main.cpp
@@ -0,0 +1,21 @@
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::array;
+using std::vector;
+
+int main(int argc, char const *argv[]) {
+
+  const char *const theString = "Test";
+  cout << theString << endl;
+  // theString[0] = 'fuck'; //cannot modify
+  // cout << theString << endl;
+  return 0;
+}
