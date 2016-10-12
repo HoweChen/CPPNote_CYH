a0d69178c3c58a35b3dc227c1596adbaa306384f
SkyFather <cyh278889789@gmail.com>
2016-10-12 15:06:37 +0800

    string.find() and string.size(), length()

---
+++ string/find/main.cpp
@@ -0,0 +1,60 @@
+#include <algorithm>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+
+#define Inf 2147483647
+#define Pi acos(-1.0)
+#define N 1000000
+#define LL long long
+
+inline LL Power(int b, int p) {
+  LL ret = 1;
+  for (int i = 1; i <= p; i++)
+    ret *= b;
+  return ret;
+}
+
+const int dr[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+const int dc[] = {0, 1, 1, 1, 0, -1, -1, -1};
+
+#define F(i, a) for (int i = (0); i < (a); i++)
+#define Fs(i, sz) for (size_t i = 0; i < sz.size(); i++)
+#define Fe(i, x) for (typeof(x.begin()) i = x.begin(); i != x.end(); i++)
+#define Set(a, s) memset(a, s, sizeof(a))
+#define Max(a, b) (a < b ? b : a)
+#define Min(a, b) (a > b ? b : a)
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+
+  //time to code
+  std::string a = "fuck";
+  std::string b = "fuckyou";
+  if (b.find(a) != string::npos) {
+    cout << "Find" << endl;
+  } else cout << "Not found" << endl;
+
+  //for string, size() and length() are the same
+  cout << a.size() << endl;
+  cout << a.length() << endl;
+
+  return 0;
+}
+
+// @END_OF_SOURCE_CODE
