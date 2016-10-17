aac7f450aa8c3cec1269405389897c1ac336762a
SkyFather <cyh278889789@gmail.com>
2016-10-18 02:30:44 +0800

    test

--- ACM-Template.cpp
+++ ACM-Template.cpp
@@ -102,3 +102,41 @@ string replace_substring_to_another_substring(string s, string substr_original,
 }
 
 // @END_OF_SOURCE_CODE
+
+
+/*
+ * Block comment
+ */
+#include <vector>
+
+using namespace std;  // line comment
+namespace foo {
+
+  typedef struct Struct {
+    int field;
+  } Typedef;
+  enum Enum {Foo = 1, Bar = 2};
+
+  Typedef *globalVar;
+  extern Typedef *externVar;
+
+  template<typename T, int N>
+  class Class {
+    T n;
+  public:
+    void function(int paramName) {
+      int *localVar = new int[1];
+      this->n = N;
+
+    label:
+      printf("Formatted string %d\n\g", localVar[0]);
+      printf(R"**(Formatted raw-string %d\n)**", 1);
+      std::cout << (1 << 2) << std::endl;
+
+    #define FOO(A) A
+    #ifdef DEBUG
+      printf("debug");
+    #endif
+    }
+  };
+}
