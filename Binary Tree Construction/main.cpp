5f9ab75a2314a4194ed9b67bb7d327dda19c564f
SkyFather <cyh278889789@gmail.com>
2016-11-03 19:57:46 +0800

    update output

--- Binary Tree Construction/main.cpp
+++ Binary Tree Construction/main.cpp
@@ -3,12 +3,14 @@
 #include <cmath>
 #include <map>
 #include <vector>
+#include <boost/multiprecision/cpp_int.hpp>
 
 using namespace std;
+namespace mp = boost::multiprecision;
 
-map<int, unsigned long long int> BinaryTreeMap;
+map<int, unsigned long long> BinaryTreeMap;
 
-//unsigned long long int BTree(int node_number);
+//unsigned long long BTree(int node_number);
 void BTree(int node_number);
 
 int main() {
@@ -16,9 +18,9 @@ int main() {
   BinaryTreeMap[1] = 1;
   BinaryTreeMap[2] = 2;
   BinaryTreeMap[3] = 5;
-  for (int i = 4; i <= 100; ++i) {
-    BTree(i);
-  }
+//  for (int i = 4; i <= 100; ++i) {
+//    BTree(i);
+//  }
 
 
   int n;
@@ -26,17 +28,17 @@ int main() {
   if (n <= 0) {
     exit(0);
   } else {
-    vector<unsigned long long int> result;
+    vector<unsigned long long> result;
     for (size_t i = 0; i < n; ++i) {
       int node_number;
       cin >> node_number;
       if (node_number < 1 || node_number > 10000) {
         exit(0);
       } else {
-        map<int, unsigned long long int>::iterator search = BinaryTreeMap.find(node_number);
+        map<int, unsigned long long>::iterator search = BinaryTreeMap.find(node_number);
         if (search != BinaryTreeMap.end()) {
           result.push_back(search->second);
-        } else{
+        } else {
           BTree(node_number);
           result.push_back(BinaryTreeMap.find(node_number)->second);
         }
@@ -47,9 +49,6 @@ int main() {
       cout << result[j] << endl;
     }
 
-//    for (int i = 4; i <= 10; ++i) {
-//      cout<<BTree(i)<<endl;
-//    }
   }
   return 0;
 }
@@ -58,7 +57,7 @@ int main() {
 void BTree(int node_number) {
   //version 2
   for (int i = 4; i <= node_number; ++i) {
-    unsigned long long int result = 0;
+    unsigned long long result = 0;
     int without_root = i - 1;
     int middle = without_root / 2;
 
@@ -67,34 +66,41 @@ void BTree(int node_number) {
       for (int k = 1; k < middle; ++k) {
         int j = without_root - k;
         result = result + 2 * BinaryTreeMap.find(k)->second * BinaryTreeMap.find(j)->second;
+        cout << i << ": 2 * F[" << k << "] * F[" << j << "] = " << result << endl;
       }
       result = result + BinaryTreeMap.find(middle)->second * BinaryTreeMap.find(middle)->second;
+      cout << i << ": F[" << middle << "] * F[" << middle << "] = " << result << endl;
       result = result + 2 * BinaryTreeMap.find(without_root)->second;
+      cout << i << ": 2 * F[" << without_root << "] = " << result << endl;
 
     } else {
 
       for (int k = 1; k <= middle; ++k) {
         int j = without_root - k;
         result = result + 2 * BinaryTreeMap.find(k)->second * BinaryTreeMap.find(j)->second;
+        cout << i << ": 2 * F[" << k << "] * F[" << j << "] = " << result << endl;
       }
       result = result + 2 * BinaryTreeMap.find(without_root)->second;
+      cout << i << ": 2 * F[" << without_root << "] = " << result << endl;
 
     }
     BinaryTreeMap[i] = result;
+//    cout<<result<<endl;
+
   }
 
 //  return BinaryTreeMap.find(node_number)->second;
 }
 
 
-//unsigned long long int BTree(int node_number) {
+//unsigned long long BTree(int node_number) {
 //
 ////  version 1
-//  map<int, unsigned long long int>::iterator search = BinaryTreeMap.find(node_number);
+//  map<int, unsigned long long>::iterator search = BinaryTreeMap.find(node_number);
 //  if (search != BinaryTreeMap.end()) {
 //    return search->second;
 //  } else {
-//    unsigned long long int result = 0;
+//    unsigned long long result = 0;
 //    int without_root = node_number - 1;
 //    int middle = without_root / 2;
 //
