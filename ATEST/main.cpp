5663d0be415ca744d9d93d0ae9ac7c8bd3db7e85
SkyFather <cyh278889789@gmail.com>
2016-11-04 20:20:20 +0800

    add cppTest folder and file with input.txt

---
+++ ATEST/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  freopen("input.txt", "r", stdin);
+  string test;
+  getline(cin, test, ' ');
+  cout<<test<<endl;
+  return 0;
+}
